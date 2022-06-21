4efc38432303eea5ce10c5b38c5f51efc7160ff6
Kristian Beall <beallkristian@gmail.com>
2022-06-20 20:06:12 -0400

    gitignore added

---
+++ animal.Repository/Animal.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace animal.Repository
+{
+    public class Animal
+    {
+        
+    }
+}
