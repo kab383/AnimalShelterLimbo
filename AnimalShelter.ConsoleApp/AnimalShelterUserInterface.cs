15317fa00df8e5ca250d28bf49cda855d84f150a
Kristian Beall <beallkristian@gmail.com>
2022-06-20 20:26:58 -0400

    condensed file structure to one interface

--- AnimalShelter.ConsoleApp/AnimalShelterUserInterface.cs
+++ AnimalShelter.ConsoleApp/AnimalShelterUserInterface.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace customer.ConsoleApp
+namespace AnimalShelter.ConsoleApp
 {
-    public class CustomerCustomConsole
+    public class AnimalShelterUserInterface
     {
         
     }
